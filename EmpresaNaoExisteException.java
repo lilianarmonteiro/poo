f45d6ca1a67194ba8dd20fd4250d0c5aefe36eab
Liliana <liliana.patricia.monteiro@hotmail.com>
2017-05-26 18:50:34 +0100

    empresataxis e EmpresaNaoExiste exception comentados

--- EmpresaNaoExisteException.java
+++ EmpresaNaoExisteException.java
@@ -1,12 +1,21 @@
-
 /**
- * Escreva a descrição da classe EmpresaNaoExisteException aqui.
+ * Classe EmpresaNaoExisteException que devolve uma mensagem caso a empresa não exista.
  * 
- * @author (seu nome) 
- * @version (número de versão ou data)
+ * @author Isabel Sofia Pereira
+ * @author Liliana Monteiro
+ * @author Maria de La Salete Teixeira
  */
 public class EmpresaNaoExisteException extends Exception
 {
+   /**
+    * Método de instância
+    */
+   
+   /**
+    * Método que define a mensagem caso a empresa não exista
+    * 
+    * @param msg    A mensagem a ser enviada
+    */
    public EmpresaNaoExisteException(String msg){
        super(msg);
    }
